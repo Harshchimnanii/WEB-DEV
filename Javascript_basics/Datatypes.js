5dd2b6e9c5cff78d1f7a50ecbf3619b2b9e07cf0
Harsh Chimnani <chimnaniharsh4@gmail.com>
2025-11-10 13:00:46 +0000

    update

---
+++ Javascript_basics/Datatypes.js
@@ -0,0 +1,40 @@
+/* Primitive Data Types */
+/* Numbers . Booleann , undefined , String , null , bigInt(2^53 - 1) , Symbol*/ 
+// let number1 = 23 ;
+// let String_name = " Harsh ";
+// let Booleann = true;
+// let undefined_age ;
+// let bigInt = 842309402048024884946949164949619494;
+// let symboll = '^';
+// let khali = null;
+// console.log(number1);
+// console.log(String_name);
+// console.log(Booleann);
+// console.log(undefined_age);
+// console.log(bigInt);
+// console.log(symboll);
+// console.log(khali);
+// /* type of */
+// console.log(typeof(number1));
+// console.log(typeof(String_name));
+// console.log(typeof(Booleann));
+// console.log(typeof(undefined_age));
+// console.log(typeof(bigInt));
+// console.log(typeof(symboll));
+// console.log(typeof(khali));
+
+
+//symbol 
+const sym1 = Symbol("tmkc")
+const sym2 = Symbol("tmkc");
+console.log(sym1 === sym2)
+console.log(typeof(sym1))
+
+
+
+
+
+
+
+
+/* Non-Primitive Data Types */
