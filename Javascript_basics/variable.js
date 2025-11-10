a35d3ebb953dba55e9f4611f030beb7c658390c2
Harsh Chimnani <chimnaniharsh4@gmail.com>
2025-11-10 12:00:14 +0000

    update

---
+++ Javascript_basics/variable.js
@@ -0,0 +1,37 @@
+// initialisation
+// var age = 22;
+// console.log("Age:", age);
+
+// fuctional scoped 
+
+// function solve(){
+//     var age = 53;
+//     console.log("Age inside function:", age);
+// }
+//  
+// solve();
+// 
+// console.log("Age outside function:", age);
+
+// Global scoped 
+// var age = 20;
+// if(true){
+//     console.log("Age inside if block:", age);
+// }
+// var can be declared again but not let it can be reassigned but not redeclared like let x = 10 ; let x = 20 not possible but let x = 10 ; x = 20 yes possible 
+// block scoped nhi hai 
+
+// lekin let jo ki es6 me aaya hai block scoped hai
+// let score = 100;
+// if(true){
+//     // let score = 200;
+//     console.log("Score inside if block:", score);
+// }
+
+
+
+
+// const ispe no change allowed 
+ 
+const x = 10 ;
+console.log(x);
