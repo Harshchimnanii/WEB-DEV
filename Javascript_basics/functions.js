86c146afe858d616b8f9461a47fa673cc0417b92
Harsh Chimnani <chimnaniharsh4@gmail.com>
2025-11-24 19:14:33 +0000

    update

--- Javascript_basics/functions.js
+++ Javascript_basics/functions.js
@@ -3,3 +3,32 @@ function helloBhai(){
     console.log("kaise ho aap")
 }
 helloBhai();
+// loop inside function 
+function loop(){
+    for(let i = 0 ; i<10 ; i++ ){
+        console.log(i);
+    }
+}
+loop();
+// paramerters passing
+function param(number){
+    
+    console.log(number +10);
+}
+param(8);
+
+
+//  calcute the average 
+
+function ret(num1 , num2){
+    average = (num1 + num2)/2;
+    return average;
+}
+let result = ret(12 , 3);
+console.log(result);
+
+//  lets take another method to initialise the function 
+let squarednumber = function (a){
+    return a**2;
+}
+console.log(squarednumber(10));
