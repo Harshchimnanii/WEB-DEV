827c8b511d4f391ab3483f5c4a93198d2d46e451
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-01-15 19:38:28 +0000

    update

--- Javascript_basics/ConversionOperation.js
+++ Javascript_basics/ConversionOperation.js
@@ -1,9 +1,21 @@
-let score =' 33abc' ;
-console.log(score);
-let valueInNumber = Number(score);
-console.log(valueInNumber);
-console.log(typeof valueInNumber);
+// let score =' 33abc' ;
+// console.log(score);
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true : 1 ; false : 0
+
+// ****************************** Operations ******************************
+
+let value  = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+
+// suspicious string 
+ console.log("1" + 2 + 2);
+ console.log(1 + 2 + "2");
+ 
