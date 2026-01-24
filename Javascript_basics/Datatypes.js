6ba8c9c7b88939eb17bec48a5a0d2ed5531a906b
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-01-24 12:11:28 +0000

    datatypes done date "24-01-2026"

--- Javascript_basics/Datatypes.js
+++ Javascript_basics/Datatypes.js
@@ -18,3 +18,41 @@ console.log(typeof(9));
 console.log(typeof(null));
 console.log(typeof(undefined));
 
+const Bignumber = 1234567890123456789012345678901234567890n;
+let harsh = 'hardsh';
+const harsh1 = Symbol('123');
+const harsh2 = Symbol('123');
+console.log(Bignumber , harsh );
+console.log(typeof(harsh1));
+console.log(typeof(harsh2));
+let help = harsh1 == harsh2;
+console.log(help);
+const heroes = ["shaktimaan" , "chacha vidhyak" , "zakir khan"];
+// objects defined in curly braces ;
+let myobj = {
+    name : "harsh",
+    age : 22,
+}
+
+// define function using a variable 
+
+const Myfirstfunc = function(){
+    console.log("hello world same as is");
+    
+}
+Myfirstfunc();
+
+// actually returns function but said a s object function 
+
+console.log(typeof Myfirstfunc);
+
+
+// ab iska bhi dekhle 
+
+
+console.log(typeof myobj);
+
+
+// null is also an object bhaii 
+
+unique wala maamla smjha 
