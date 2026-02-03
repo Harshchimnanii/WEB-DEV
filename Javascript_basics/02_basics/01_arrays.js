9911e91ae01db9738131964a3c1c0360c9f4af0e
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-02-03 20:10:56 +0000

    array completed

--- Javascript_basics/02_basics/01_arrays.js
+++ Javascript_basics/02_basics/01_arrays.js
@@ -1,4 +1,17 @@
 // array
 
-const arr = [1 , 2, ,3 ,4 ,5 ,6, 7, 8 , "true" , true , 0.90 ];
+const arr = [1 , 2,3 ,4 ,5 ,6, 7, 8 , "true" , true , 0.90 ];
 console.log(arr);
+const arr1 = new Array(1 ,2 ,3 ,4);
+console.log(arr1);
+
+arr1.push(5);
+console.log(arr1);
+arr1.push(6);
+console.log(arr1);
+arr1.pop();
+console.log(arr1);
+arr1.unshift(9);
+console.log(arr1);
+arr1.shift();
+console.log(arr1);
